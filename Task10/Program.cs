2dfc014b7d59b473777782356e64c0b72bd78900
Greentima <blinovtimofey@gmail.com>
2022-08-15 21:38:45 +0300

    HW solve commit

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int FindSecondNumber(int number) 
+{
+    int FindNum = number / 10;
+    FindNum = FindNum % 10;
+
+    return FindNum;
+}
+
+Console.WriteLine("Input 3 chracter number ");
+int NewNum = Convert.ToInt32(Console.ReadLine());
+NewNum = FindSecondNumber(NewNum);
+Console.WriteLine($"Second number is {NewNum}");
---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//
+//Неявно с какого конца, и в тестах тоже, поэтому 3ю с конца вывожу
+
+int FindThirdNumber(int number) 
+{
+    int TirdNum;
+    if (number > 99)
+    
+        TirdNum = ((number / 100) % 10);
+    else
+        TirdNum =  -1;  
+
+    return TirdNum;     
+}
+
+Console.WriteLine("Input number ");
+int NewNum = Convert.ToInt32(Console.ReadLine());
+NewNum = FindThirdNumber(NewNum);
+
+if (NewNum == -1)
+    Console.WriteLine("третьей цифры нет");    
+else
+    Console.WriteLine($"{NewNum}");     
+
+
+
